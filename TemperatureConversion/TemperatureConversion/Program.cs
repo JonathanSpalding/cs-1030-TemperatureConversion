ee4f49d8487d94833ccacf4cbaeafb405c8bb22f
JonathanSpalding <Jon.Spalding@hotmail.com>
2016-04-20 11:17:14 -0600

    CS 1030 C#

---
+++ TemperatureConversion/TemperatureConversion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConversion
+{
+    class Program
+    {
+        static int ToCelsius(int f)
+        {
+            int C;
+            C = (int)(5.0f / 9.0f * ((float)f - 32.0f));
+            return C;
+        }
+
+        static string Input(string Prompt)
+        {
+            Console.Write("What is the temperature in Fahrenheit? ");
+            string textLine = Console.ReadLine();
+            return textLine;
+        }
+
+        static void Main(string[] args)
+        {
+            String textLine, outLine;
+            int celsius, fahrenheit;
+
+            do
+            {
+                textLine = Input("What is the temperature in Fahrenheit? ");
+                if (textLine != "")
+                {
+                    fahrenheit = int.Parse(textLine);
+                    celsius = ToCelsius(fahrenheit);
+                    outLine = (String.Format("Temperature in Celsius: {0:N0}", celsius.ToString()));
+                    Console.WriteLine(outLine);
+                }
+            } while (textLine != "");
+        }
+    }
+}
